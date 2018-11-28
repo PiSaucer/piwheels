8b3e3239dc01ea17c558dab8d9503ba3b838b838
PiSaucer <33438654+PiSaucer@users.noreply.github.com>
2018-11-27 21:21:36 -1000

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<object type="application/x-shockwave-flash" data="Happy-Wheels.swf" width="400" height="50"> 
+<object type="application/x-shockwave-flash" data="Happy-Wheels.swf" width="900" height="500"> 
   <param name="movie" value="Happy-Wheels.swf" />
   <param name="quality" value="high" />
 </object>
